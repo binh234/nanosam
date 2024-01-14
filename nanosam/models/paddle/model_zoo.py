e760d16d6d146672ece7c23c7f7acbf427ef53ff
Binh Le <binhnd234@gmail.com>
2024-01-14 09:53:18 +0700

    Add middle_op and head_depth as optional params

--- nanosam/models/paddle/model_zoo.py
+++ nanosam/models/paddle/model_zoo.py
@@ -51,180 +51,187 @@ class ImageEncoder(nn.Layer):
         return out
 
 
-def Sam_PPHGNetV2_B0(**kwargs):
+def Sam_PPHGNetV2_B0(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPHGNetV2Backbone_B0(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1024, 512, 256],
         head_width=256,
-        head_depth=4,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=True,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B1(**kwargs):
+def Sam_PPHGNetV2_B1(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPHGNetV2Backbone_B1(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1024, 512, 256],
         head_width=256,
-        head_depth=4,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=True,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B2(**kwargs):
+def Sam_PPHGNetV2_B2(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPHGNetV2Backbone_B2(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1536, 768, 384],
         head_width=256,
-        head_depth=6,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=True,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B3(**kwargs):
+def Sam_PPHGNetV2_B3(middle_op="fmbconv", head_depth=5, **kwargs):
     backbone = PPHGNetV2Backbone_B3(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[2048, 1024, 512],
         head_width=256,
-        head_depth=6,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=True,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B4(**kwargs):
+def Sam_PPHGNetV2_B4(middle_op="fmbconv", head_depth=6, **kwargs):
     backbone = PPHGNetV2Backbone_B4(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[2048, 1024, 512],
         head_width=256,
-        head_depth=8,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=False,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B5(**kwargs):
+def Sam_PPHGNetV2_B5(middle_op="fmbconv", head_depth=6, **kwargs):
     backbone = PPHGNetV2Backbone_B5(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[2048, 1024, 512],
         head_width=256,
-        head_depth=8,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=False,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNetV2_B6(**kwargs):
+def Sam_PPHGNetV2_B6(middle_op="fmbconv", head_depth=6, **kwargs):
     backbone = PPHGNetV2Backbone_B6(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[2048, 1024, 512],
         head_width=256,
-        head_depth=12,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
+        use_lab=False,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNet_tiny(**kwargs):
+def Sam_PPHGNet_tiny(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPHGNetBackbone_tiny(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[768, 512, 448],
         head_width=256,
-        head_depth=4,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPHGNet_small(**kwargs):
+def Sam_PPHGNet_small(middle_op="fmbconv", head_depth=6, **kwargs):
     backbone = PPHGNetBackbone_small(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1024, 768, 512],
         head_width=256,
-        head_depth=8,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPLCNetV2_small(**kwargs):
+def Sam_PPLCNetV2_small(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPLCNetV2Backbone_small(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[768, 384, 192],
         head_width=256,
-        head_depth=4,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPLCNetV2_base(**kwargs):
+def Sam_PPLCNetV2_base(middle_op="fmbconv", head_depth=4, **kwargs):
     backbone = PPLCNetV2Backbone_base(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1024, 512, 256],
         head_width=256,
-        head_depth=4,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
     )
 
     model = ImageEncoder(backbone, neck)
     return model
 
 
-def Sam_PPLCNetV2_large(**kwargs):
+def Sam_PPLCNetV2_large(middle_op="fmbconv", head_depth=6, **kwargs):
     backbone = PPLCNetV2Backbone_large(**kwargs)
     neck = SamNeck(
         fid_list=["stage4", "stage3", "stage2"],
         in_channel_list=[1280, 640, 320],
         head_width=256,
-        head_depth=8,
+        head_depth=head_depth,
         expand_ratio=1,
-        middle_op="fmbconv",
+        middle_op=middle_op,
     )
 
     model = ImageEncoder(backbone, neck)
