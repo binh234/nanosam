9b6b7be6f786e00b2b0e5cae9c293377bf22bc6a
Binh Le <binhnd234@gmail.com>
2024-01-25 19:45:05 +0700

    Remove torch2trt dependency

--- scripts/extract_sam_features_trt.py
+++ scripts/extract_sam_features_trt.py
@@ -1,93 +1,105 @@
 import numpy as np
+import cv2
 import torch
-import torch.nn.functional as F
-from torchvision.transforms.functional import resize, to_pil_image  # type: ignore
 
 import argparse
 import glob
 import os
 import tensorrt as trt
-from typing import Tuple
 from PIL import Image
-from torch2trt import TRTModule
 from tqdm import tqdm
 
 IMG_EXTENSIONS = {".jpg", ".jpeg", ".png"}
 
 
-class ResizeLongestSide:
-    """
-    Resizes images to the longest side 'target_length', as well as provides
-    methods for resizing coordinates and boxes. Provides methods for
-    transforming both numpy array and batched torch tensors.
-    """
+class TrtModel:
+    def __init__(self, path, **kwargs):
+        """Initialize TensorRT plugins, engine and conetxt."""
+        self.load_engine(path)
 
-    def __init__(self, target_length: int) -> None:
-        self.target_length = target_length
+    def load_engine(self, path):
+        with trt.Logger() as logger, trt.Runtime(logger) as runtime:
+            with open(path, "rb") as f:
+                engine_bytes = f.read()
+            self.engine = runtime.deserialize_cuda_engine(engine_bytes)
+        self.get_inference_info(self.engine)
 
-    def apply_image(self, image: np.ndarray) -> np.ndarray:
-        """
-        Expects a numpy array with shape HxWxC in uint8 format.
-        """
-        target_size = self.get_preprocess_shape(image.shape[0], image.shape[1], self.target_length)
-        return np.array(resize(to_pil_image(image), target_size))
+    def get_inference_info(self, engine):
+        self.context = engine.create_execution_context()
+        self.stream = torch.cuda.current_stream()
+        self.input_shapes = []
+        self.output_shapes = []
+        self.binding_outputs = []
+        self.outputs = []
 
-    @staticmethod
-    def get_preprocess_shape(oldh: int, oldw: int, long_side_length: int) -> Tuple[int, int]:
-        """
-        Compute the output size given input size and target long side length.
-        """
-        scale = long_side_length * 1.0 / max(oldh, oldw)
-        newh, neww = oldh * scale, oldw * scale
-        neww = int(neww + 0.5)
-        newh = int(newh + 0.5)
-        return (newh, neww)
+        for binding in engine:
+            shape = engine.get_tensor_shape(binding)
+            dtype = trt.nptype(engine.get_tensor_dtype(binding))
+
+            if engine.get_tensor_mode(binding) == trt.TensorIOMode.INPUT:
+                self.input_shapes.append(shape)
+            else:
+                tensor = torch.from_numpy(np.empty(shape, dtype=np.dtype(dtype))).cuda()
+                self.binding_outputs.append(tensor.data_ptr())
+                self.outputs.append(tensor)
+                self.output_shapes.append(shape)
+
+    def infer(self, *args):
+        """Run inference on TensorRT engine."""
+        assert len(args) == len(
+            self.input_shapes
+        ), "Number of arguments must match number of model's input"
+
+        binding_outputs = [inp.data_ptr() for inp in args] + self.binding_outputs
+        self.context.execute_async_v2(
+            bindings=binding_outputs, stream_handle=self.stream.cuda_stream
+        )
+        self.stream.synchronize()
+
+        return self.outputs  # torch tensors on GPU
+
+    def __call__(self, *args):
+        out = self.infer(*args)
+
+        return out
 
 
-def load_image_encoder_engine(path: str):
-    with trt.Logger() as logger, trt.Runtime(logger) as runtime:
-        with open(path, "rb") as f:
-            engine_bytes = f.read()
-        engine = runtime.deserialize_cuda_engine(engine_bytes)
+def preprocess_image(image, size: int = 512):
+    if not isinstance(image, np.ndarray):
+        image = np.asarray(image)
 
-    image_encoder_trt = TRTModule(
-        engine=engine, input_names=["image"], output_names=["image_embeddings"]
-    )
+    image_mean = np.asarray([123.675, 116.28, 103.53])[:, None, None]
+    image_std = np.asarray([58.395, 57.12, 57.375])[:, None, None]
 
-    return image_encoder_trt
+    height, width = image.shape[:2]
+    aspect_ratio = width / height
+    if aspect_ratio >= 1:
+        resize_width = size
+        resize_height = int(size / aspect_ratio)
+    else:
+        resize_height = size
+        resize_width = int(size * aspect_ratio)
 
+    image_np_resized = cv2.resize(np.asarray(image), (resize_width, resize_height))
+    image_np_resized = np.transpose(image_np_resized, (2, 0, 1))
+    image_np_resized_normalized = (image_np_resized - image_mean) / image_std
+    image_tensor = np.zeros((3, size, size), dtype=np.float32)
+    image_tensor[:, :resize_height, :resize_width] = image_np_resized_normalized
 
-def transform(x, img_size=1024):
-    """Normalize pixel values and pad to a square input."""
-    # Normalize colors
-    transform = ResizeLongestSide(img_size)
-    x = transform.apply_image(x)
-    x = torch.from_numpy(x)
-    x = x.permute(2, 0, 1).contiguous()
-
-    pixel_mean = torch.Tensor([123.675, 116.28, 103.53]).view(-1, 1, 1)
-    pixel_std = torch.Tensor([58.395, 57.12, 57.375]).view(-1, 1, 1)
-    x = (x - pixel_mean) / pixel_std
-
-    # Pad
-    h, w = x.shape[-2:]
-    padh = img_size - h
-    padw = img_size - w
-    x = F.pad(x, (0, padw, 0, padh))
-    return x
+    return image_tensor
 
 
 def extract_feat_and_save(image_encoder, images, filenames, args):
-    inp_tensor = torch.stack(images, 0)
+    inp_tensor = np.stack(images, 0)
     if len(filenames) < args.batch_size:
-        pad_tensor = torch.zeros(
+        pad_tensor = np.zeros(
             (args.batch_size - len(filenames), *inp_tensor.shape[1:]), dtype=inp_tensor.dtype
         )
-        inp_tensor = torch.cat((inp_tensor, pad_tensor), 0)
-    inp_tensor = inp_tensor.cuda()
+        inp_tensor = np.concatenate((inp_tensor, pad_tensor), 0)
+    inp_tensor = torch.as_tensor(inp_tensor).cuda()
 
     with torch.no_grad():
-        features = image_encoder(inp_tensor)
+        features = image_encoder(inp_tensor)[0]
 
     features = features.cpu().numpy()
     if args.fp16:
@@ -145,7 +157,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    image_encoder = load_image_encoder_engine(args.image_encoder)
+    image_encoder = TrtModel(args.image_encoder)
 
     os.makedirs(args.out_dir, exist_ok=True)
     image_paths = glob.iglob(os.path.join(args.img_dir, "*"))
@@ -157,7 +169,7 @@ if __name__ == "__main__":
             filename, image_ext = os.path.splitext(basename)
             if image_ext in IMG_EXTENSIONS:
                 image = np.asarray(Image.open(image_path).convert("RGB"))
-                image = transform(image, args.image_size)
+                image = preprocess_image(image, args.image_size)
                 batch_images.append(image)
                 batch_filenames.append(filename)
 
