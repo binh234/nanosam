8b42767fd8a7f173ab39dc4a5409ec3449eea8a2
Binh Le <binhnd234@gmail.com>
2024-03-12 19:21:10 +0700

    Update PTQ script

--- nanosam/tools/paddle/quant_post_static.py
+++ nanosam/tools/paddle/quant_post_static.py
@@ -1,13 +1,35 @@
 from __future__ import absolute_import, division, print_function
 import numpy as np
 
+from nanosam.tools.paddle.dataset import ImageFolderDataset, SamImageFolderDataset
+
+import copy
 import os
 import paddle
 import paddleslim
+from paddle.io import DataLoader, Dataset
 from ppcls.utils import config as conf
 from ppcls.utils.logger import init_logger
 
-from nanosam.tools.paddle.loader import build_dataloader
+
+class DatasetWrapper(Dataset):
+    def __init__(self, dataset, input_name="image"):
+        self.dataset = dataset
+        self.input_name = input_name
+
+    def try_getitem(self, index):
+        return {self.input_name: self.dataset[index][0]}
+
+    def __getitem__(self, index):
+        try:
+            return self.try_getitem(index)
+        except Exception as ex:
+            print("Exception occured with msg: {}".format(ex))
+            rnd_idx = np.random.randint(self.__len__())
+            return self.__getitem__(rnd_idx)
+
+    def __len__(self):
+        return len(self.dataset)
 
 
 def main():
@@ -21,24 +43,26 @@ def main():
     )
     if "Query" in config["DataLoader"]["Eval"]:
         config["DataLoader"]["Eval"] = config["DataLoader"]["Eval"]["Query"]
-    config["DataLoader"]["Eval"]["sampler"]["batch_size"] = 1
-    config["DataLoader"]["Eval"]["loader"]["num_workers"] = 0
 
     init_logger()
-    device = paddle.set_device("cpu")
-    train_dataloader = build_dataloader(config["DataLoader"], "Eval", device, False)
+    device = paddle.set_device(config["Global"]["device"])
 
-    def sample_generator(loader):
-        def __reader__():
-            for indx, data in enumerate(loader):
-                images = np.array(data[0])
-                yield images
-
-        return __reader__
+    config_dataset = config["DataLoader"]["Eval"]["dataset"]
+    batch_size = 1
+    config_dataset = copy.deepcopy(config_dataset)
+    dataset_name = config_dataset.pop("name")
+    dataset = eval(dataset_name)(**config_dataset)
+    dataset = DatasetWrapper(dataset)
+    dataloader = DataLoader(
+        dataset=dataset,
+        places=device,
+        num_workers=0,
+        batch_size=batch_size,
+        shuffle=False,
+    )
 
     paddle.enable_static()
-    place = paddle.CPUPlace()
-    exe = paddle.static.Executor(place)
+    exe = paddle.static.Executor(device)
     paddleslim.quant.quant_post_static(
         executor=exe,
         model_dir=config["Global"]["save_inference_dir"],
@@ -47,8 +71,8 @@ def main():
         quantize_model_path=os.path.join(
             config["Global"]["save_inference_dir"], "quant_post_static_model"
         ),
-        sample_generator=sample_generator(train_dataloader),
-        batch_size=config["DataLoader"]["Eval"]["sampler"]["batch_size"],
+        data_loader=dataloader,
+        batch_size=batch_size,
         batch_nums=10,
     )
 
