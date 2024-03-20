4e139bf710bf25a8c06556970de56ea75e96cb61
Binh Le <binhnd234@gmail.com>
2024-03-20 11:52:11 +0700

    Update onnx export

--- nanosam/mobile_sam/modeling/mask_decoder.py
+++ nanosam/mobile_sam/modeling/mask_decoder.py
@@ -123,9 +123,15 @@ class MaskDecoder(nn.Module):
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
 
         # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+        if torch.onnx.is_in_onnx_export():
+            pos_src = image_pe.repeat([tokens.shape[0]] + [1] * (len(image_pe.shape) - 1))
+            src = image_embeddings.repeat(
+                [tokens.shape[0]] + [1] * (len(image_embeddings.shape) - 1)
+            )
+        else:
+            pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+            src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
         src = src + dense_prompt_embeddings
-        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
         b, c, h, w = src.shape
 
         # Run the transformer
